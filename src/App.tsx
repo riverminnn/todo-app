6b50fdb565e9a5e13452168f85dd35b6b8d014f3
river <riverminnn@gmail.com>
2025-03-10 22:30:17 +0700

    Step 2: Add Library Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-     
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
+      <h2 className='text-xl'>Hello World</h2>
     </>
   )
 }
